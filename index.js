f8c099fd45d4290846b70801742c6903aac10f6a
MarchWaden <max.linz@live.com>
2018-10-11 12:11:46 -0600

    will now send pokemon array on index route

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const app = express();
+const Pokemon = require('./model/pokemon.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
+app.get('/pokemon', (req, res) => {
+  res.send(Pokemon)
+});
 
 app.listen(3000, () => {
   console.log('server is listening on port 3000')
